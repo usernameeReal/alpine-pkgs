2fa0d6fadc48adbc1ad018477fb99a9438eecafe
usewnamwee <86745494+usernameeReal@users.noreply.github.com>
2023-04-15 15:13:20 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ then
 echo "Private key missing."
 exit 1
 fi
+echo "Installing stuff..."
 apk add -U alpine-sdk
 if [ "$(id -u)" = "0" ]
 then
@@ -13,12 +14,14 @@ adduser -D notroot
 addgroup notroot abuild
 exec su notroot -c "sh $0"
 fi
+echo "Setting up abuild..."
 mkdir -p $HOME/.abuild
 echo 'PACKAGER_PRIVKEY="/tmp/pkey"' > $HOME/.abuild/abuild.conf
 echo "$APKSIGN_PRIVATE_KEY" > /tmp/pkey
 for pkg in baz
 do
-cd $pkg
+echo "Building $pkg"
+cd "$pkg"
 abuild -r
 cd ..
 cp $HOME/packages/*.apk .
